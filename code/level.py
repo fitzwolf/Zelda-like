fbd64cb5516738882f7664982d659779b167e37b
fitzwolf <danfelker@gmail.com>
2022-04-10 22:56:18 -0600

    enemy logic basics added

--- code/level.py
+++ code/level.py
@@ -7,105 +7,121 @@ from support import *
 from random import choice
 from weapon import Weapon
 from ui import UI
+from enemy import Enemy
 
 class Level:
-	def __init__(self):
+    def __init__(self):
 
-		# get the display surface 
-		self.display_surface = pygame.display.get_surface()
+        # get the display surface 
+        self.display_surface = pygame.display.get_surface()
 
-		# sprite group setup
-		self.visible_sprites = YSortCameraGroup()
-		self.obstacle_sprites = pygame.sprite.Group()
+        # sprite group setup
+        self.visible_sprites = YSortCameraGroup()
+        self.obstacle_sprites = pygame.sprite.Group()
 
-		# sprite setup
-		self.create_map()
+        # sprite setup
+        self.create_map()
 
-		# attack sprites
-		self.current_attack = None
-		self.ui = UI()
+        # attack sprites
+        self.current_attack = None
+        self.ui = UI()
 
 
-	def create_map(self):
-		layouts = {
-			'boundary':import_csv_layout(BASEDIR+'map/map_FloorBlocks.csv'),
-			'grass':import_csv_layout(BASEDIR+'map/map_Grass.csv'),
-			'object':import_csv_layout(BASEDIR+'map/map_Objects.csv'),
-		}
+    def create_map(self):
+        layouts = {
+            'boundary':import_csv_layout(BASEDIR+'map/map_FloorBlocks.csv'),
+            'grass':import_csv_layout(BASEDIR+'map/map_Grass.csv'),
+            'object':import_csv_layout(BASEDIR+'map/map_Objects.csv'),
+            'entities':import_csv_layout(BASEDIR+'map/map_Entities.csv'),
+        }
 
-		graphics = {
-			'grass':import_folder(BASEDIR+'graphics/Grass'),
-			'objects':import_folder(BASEDIR+'graphics/objects'),
-		}
-		for style,layout in layouts.items():
-			for row_index,row in enumerate(layout):
-				for col_index, col in enumerate(row):
-					if col != '-1':
-						x = col_index * TILESIZE
-						y = row_index * TILESIZE
-						if style == 'boundary':
-		 					Tile((x,y),[self.obstacle_sprites],'invisible')
-						if style == 'grass':
-							random_grass_image = choice(graphics['grass'])
-							Tile((x,y),[self.visible_sprites,self.obstacle_sprites],'grass',surface = random_grass_image)
-							# load grass object
-						if style == 'object':
-							surf = graphics['objects'][int(col)]
-							Tile((x,y),[self.visible_sprites,self.obstacle_sprites],'object',surface = surf)
-							# load object object
-		 		# if col == 'p':
-		 		# 	self.player = Player((x,y),[self.visible_sprites],self.obstacle_sprites)
-		self.player = Player(
-				(2000,1430),
-				[self.visible_sprites],
-				self.obstacle_sprites,
-				self.create_attack,
-				self.destroy_attack,
-				self.create_magic)
+        graphics = {
+            'grass':import_folder(BASEDIR+'graphics/Grass'),
+            'objects':import_folder(BASEDIR+'graphics/objects'),
+        }
+        
+        for style,layout in layouts.items():
+            for row_index,row in enumerate(layout):
+                for col_index, col in enumerate(row):
+                    if col != '-1':
+                        x = col_index * TILESIZE
+                        y = row_index * TILESIZE
+                        if style == 'boundary':
+                            Tile((x,y),[self.obstacle_sprites],'invisible')
+                        if style == 'grass':
+                            random_grass_image = choice(graphics['grass'])
+                            Tile((x,y),[self.visible_sprites,self.obstacle_sprites],'grass',surface = random_grass_image)
+                            # load grass object
+                        if style == 'object':
+                            surf = graphics['objects'][int(col)]
+                            Tile((x,y),[self.visible_sprites,self.obstacle_sprites],'object',surface = surf)
+                            # load object object
+                        if style == 'entities':
+                            if col == '394':
+                                self.player = Player(
+                                    (x,y),
+                                    [self.visible_sprites],
+                                    self.obstacle_sprites,
+                                    self.create_attack,
+                                    self.destroy_attack,
+                                    self.create_magic)
+                            else:
+                                if col == '390': monster_name = 'bamboo'
+                                elif col == '391': monster_name = 'spirit'
+                                elif col == '392': monster_name = 'raccoon'
+                                else: monster_name = 'squid'
+                                Enemy(monster_name,(x,y),[self.visible_sprites],self.obstacle_sprites)
 
-	def create_magic(self,style,strength,cost):
-		print(style)
-		print(strength)
-		print(cost)
+                 
+    def create_magic(self,style,strength,cost):
+        print(style)
+        print(strength)
+        print(cost)
 
-	def create_attack(self):
-		self.current_attack = Weapon(self.player, [self.visible_sprites])
-	def destroy_attack(self):
-		if self.current_attack:
-			self.current_attack.kill()
-		self.current_attack = None
+    def create_attack(self):
+        self.current_attack = Weapon(self.player, [self.visible_sprites])
+    def destroy_attack(self):
+        if self.current_attack:
+            self.current_attack.kill()
+        self.current_attack = None
 
-	def run(self):
-		# update and draw the game
-		self.visible_sprites.custom_draw(self.player)
-		self.visible_sprites.update()
-		self.ui.display(self.player)
+    def run(self):
+        # update and draw the game
+        self.visible_sprites.custom_draw(self.player)
+        self.visible_sprites.update()
+        self.visible_sprites.enemy_update(self.player)
+        self.ui.display(self.player)
 
 
 class YSortCameraGroup(pygame.sprite.Group):
-	# general idea here is with a custom camera, can use an offset to give a visual overlap to the look
-	def __init__(self):
-		# general setup
-		super().__init__()
-		self.display_surface = pygame.display.get_surface()
-		self.half_width = self.display_surface.get_size()[0] // 2
-		self.half_height = self.display_surface.get_size()[1] // 2
-		self.offset = pygame.math.Vector2()
+    # general idea here is with a custom camera, can use an offset to give a visual overlap to the look
+    def __init__(self):
+        # general setup
+        super().__init__()
+        self.display_surface = pygame.display.get_surface()
+        self.half_width = self.display_surface.get_size()[0] // 2
+        self.half_height = self.display_surface.get_size()[1] // 2
+        self.offset = pygame.math.Vector2()
 
-		# creating floor map
-		self.floor_surf = pygame.image.load(BASEDIR+'graphics/tilemap/ground.png').convert()
-		self.floor_rect = self.floor_surf.get_rect(topleft = (0,0)) 
+        # creating floor map
+        self.floor_surf = pygame.image.load(BASEDIR+'graphics/tilemap/ground.png').convert()
+        self.floor_rect = self.floor_surf.get_rect(topleft = (0,0)) 
 
-	def custom_draw(self,player):
-		# getting offset
-		self.offset.x = player.rect.centerx - self.half_width
-		self.offset.y = player.rect.centery  - self.half_height
+    def custom_draw(self,player):
+        # getting offset
+        self.offset.x = player.rect.centerx - self.half_width
+        self.offset.y = player.rect.centery  - self.half_height
 
-		# drawing the floor
-		floor_offset_pos = self.floor_rect.topleft - self.offset
-		self.display_surface.blit(self.floor_surf, floor_offset_pos)
+        # drawing the floor
+        floor_offset_pos = self.floor_rect.topleft - self.offset
+        self.display_surface.blit(self.floor_surf, floor_offset_pos)
 
-		# drawing sprites
-		for sprite in sorted(self.sprites(),key = lambda sprite: sprite.rect.centery):
-			offset_pos = sprite.rect.topleft - self.offset
-			self.display_surface.blit(sprite.image,offset_pos)
+        # drawing sprites
+        for sprite in sorted(self.sprites(),key = lambda sprite: sprite.rect.centery):
+            offset_pos = sprite.rect.topleft - self.offset
+            self.display_surface.blit(sprite.image,offset_pos)
+
+    def enemy_update(self,player):
+        enemy_sprites = [sprite for sprite in self.sprites() if hasattr(sprite,'sprite_type') and sprite.sprite_type == 'enemy']
+        for enemy in enemy_sprites:
+            enemy.enemy_update(player)
--- code/player.py
+++ code/player.py
@@ -1,10 +1,9 @@
-from tkinter.ttk import Style
-from tokenize import ContStr
+from entity import Entity
 import pygame
 from support import import_folder 
 from settings import *
 
-class Player(pygame.sprite.Sprite):
+class Player(Entity):
 	def __init__(self,pos,groups,obstacle_sprites,create_attack,destroy_attack,create_magic):
 		super().__init__(groups)
 		self.image = pygame.image.load(BASEDIR+'/graphics/test/player.png').convert_alpha()
@@ -14,11 +13,9 @@ class Player(pygame.sprite.Sprite):
 		#graphics
 		self.import_player_assets()
 		self.status = 'down'
-		self.frame_index = 0
-		self.animation_speed = 0.15
+		
 
 		# vector for player movement direction
-		self.direction = pygame.math.Vector2()
 		self.obstacle_sprites = obstacle_sprites
 		
 		# movement
@@ -140,37 +137,7 @@ class Player(pygame.sprite.Sprite):
 		if not self.can_switch_magic:
 			if current_time - self.magic_switch_time >= self.switch_duration_cooldown:
 				self.can_switch_magic = True
-
-	def move(self, speed):
-		# this helps to account for angular movement
-		# if statement just makes sure we don't have an angle of zero, would cause an error
-		if self.direction.magnitude() != 0:
-			self.direction = self.direction.normalize()
-		
-		self.hitbox.x += self.direction.x * speed
-		self.collision('horizonal')
-		self.hitbox.y += self.direction.y * speed
-		self.collision('vertical')
-		self.rect.center = self.hitbox.center
-
-	def collision(self,direction):
-		if direction == 'horizonal':
-			for sprite in self.obstacle_sprites:
-				if sprite.hitbox.colliderect(self.hitbox):
-					if self.direction.x > 0: # moving right
-						self.hitbox.right = sprite.hitbox.left
-					if self.direction.x < 0: # moving left
-						self.hitbox.left = sprite.hitbox.right
-
-
-		if direction == 'vertical':
-			for sprite in self.obstacle_sprites:
-				if sprite.hitbox.colliderect(self.hitbox):
-					if self.direction.y > 0: # moving down
-						self.hitbox.bottom = sprite.hitbox.top
-					if self.direction.y < 0: # moving up
-						self.hitbox.top = sprite.hitbox.bottom
-						
+					
 	def import_player_assets(self):
 		character_path = BASEDIR + 'graphics/player/'
 		self.animations = {'up':[],'down':[],'left':[],'right':[],
